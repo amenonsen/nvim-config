c1433b6f1b4a4ebb36c70093da501db01cde3929
Abhijit Menon-Sen <ams@toroid.org>
2021-08-13 12:34:10 +0530

    Use dropdown theme with no previewer for ^F
    Note, find_files and git_files are unaffected, this is only the custom
    function I'm mapping ^F to.

--- lua/telescope-files.lua
+++ lua/telescope-files.lua
@@ -2,10 +2,13 @@
 -- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#falling-back-to-find_files-if-git_files-cant-find-a-git-directory
 --
 
+local T = require('telescope.builtin')
+local dropdown = require('telescope.themes').get_dropdown({ previewer = false })
+
 return {
     project_files = function()
-        if not pcall(require('telescope.builtin').git_files, {}) then
-            require('telescope.builtin').find_files({})
+        if not pcall(T.git_files, dropdown) then
+            T.find_files(dropdown)
         end
     end
 }
