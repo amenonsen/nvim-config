a51b7aafbc1807c27c336af1d05e8770b8a7f60b
Abhijit Menon-Sen <ams@toroid.org>
2021-08-21 13:41:06 +0530

    Omit the unchanged which-key default from .setup call

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -28,20 +28,9 @@ local packer_startup = function(use)
         config = function()
             require('which-key').setup({
                 plugins = {
-                    marks = true,
-                    registers = true,
                     spelling = {
                         enabled = true,
                         suggestions = 20,
-                    },
-                    presets = {
-                        operators = true,
-                        motions = true,
-                        text_objects = true,
-                        windows = true,
-                        nav = true,
-                        z = true,
-                        g = true,
                     },
                 },
             })
