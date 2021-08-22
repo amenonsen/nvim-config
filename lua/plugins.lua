04926234d2af5281206b67a5e6e53aec4fed424d
Abhijit Menon-Sen <ams@toroid.org>
2021-08-22 22:56:15 +0530

    Don't expand snippets on Tab
    We want snippet expansion to be initiated only through C-Space via cmp,
    so when we're trying to determine what Tab should do, it should only
    ever invoke <Plug>luasnip-jump-next, not expand-or-jump.
    
    Otherwise if we happen to type the name of an existing snippet at some
    tabstop within another snippet, hitting tab will expand the new snippet
    without having to type C-Space.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -438,8 +438,8 @@ local packer_startup = function(use)
                     ['<Tab>'] = cmp.mapping.mode({ 'i', 's' }, function(_, fallback)
                         if vim.fn.pumvisible() == 1 then
                             vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
-                        elseif luasnip.expand_or_jumpable() then
-                            vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+                        elseif luasnip.jumpable(1) then
+                            vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-next', true, true, true), '')
                         else
                             fallback()
                         end
