6693cd10b8b25343d9f43f5c72a4732a24920b76
Abhijit Menon-Sen <ams@toroid.org>
2021-08-22 14:51:41 +0530

    Hoist table of treesitter statusline transforms
    Also use setmetatable/__index to provide a default fallback instead of
    `transforms[vim.o.filetype] or function(s) …`.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -824,6 +824,27 @@ local packer_startup = function(use)
                 }
             }
 
+            local ts_stat_transforms = {
+                python = function(s)
+                    -- "class Xyzzy(object):" → "Xyzzy"
+                    if s:find("class ") then
+                        s = s:gsub("class ", ""):gsub("%([^%)]*%):", "")
+                    -- "def fn(a,r,g,s) -> r:" → "fn()"
+                    elseif s:find("def ") then
+                        s = s:gsub("def ", ""):gsub("%([^%)]*%)", "()"):gsub("%).*:", ")")
+                    end
+                    return s
+                end,
+                lua = function(s)
+                    return s:gsub(" *{$", ""):gsub("%($", "")
+                end
+            }
+            setmetatable(ts_stat_transforms, {
+                __index = function(_, _)
+                    return function(s) return s end
+                end
+            })
+
             require('lualine').setup({
                 options = {
                     theme = custom_theme,
@@ -838,28 +859,10 @@ local packer_startup = function(use)
                     },
                     lualine_b = {
                         function()
-                            local transforms = {
-                                python = function(s)
-                                    -- "class Xyzzy(object):" → "Xyzzy"
-                                    if s:find("class ") then
-                                        s = s:gsub("class ", ""):gsub("%([^%)]*%):", "")
-                                        -- "def fn(a,r,g,s) -> r:" → "fn()"
-                                    elseif s:find("def ") then
-                                        s = s:gsub("def ", ""):gsub("%([^%)]*%)", "()"):gsub("%).*:", ")")
-                                    end
-                                    return s
-                                end,
-                                lua = function(s)
-                                    return s:gsub(" *{$", ""):gsub("%($", "")
-                                end
-                            }
-
                             return require('nvim-treesitter').statusline({
                                 indicator_size = 100,
                                 type_patterns = {'class', 'function', 'method'},
-                                transform_fn = transforms[vim.o.filetype] or function(s)
-                                    return s
-                                end,
+                                transform_fn = ts_stat_transforms[vim.o.filetype],
                                 separator = ' → ',
                             })
                         end
