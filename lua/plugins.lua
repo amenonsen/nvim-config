03b210d9fca71e7d3539595168a2deeedfca08be
Abhijit Menon-Sen <ams@toroid.org>
2021-08-27 07:10:20 +0530

    Reduce kommentary configuration using new table feature

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -289,25 +289,7 @@ local packer_startup = function(use)
     use {
         'b3nj5m1n/kommentary',
         config = function()
-            require('kommentary.config').configure_language("default", {
-                ignore_whitespace = false,
-                use_consistent_indentation = true,
-                single_line_comment_string = 'auto',
-                multi_line_comment_strings = 'auto',
-                hook_function = function()
-                    require('ts_context_commentstring.internal').update_commentstring()
-                end
-            })
-            require('kommentary.config').configure_language("html", {
-                ignore_whitespace = false,
-                use_consistent_indentation = true,
-                single_line_comment_string = 'auto',
-                multi_line_comment_strings = 'auto',
-                hook_function = function()
-                    require('ts_context_commentstring.internal').update_commentstring()
-                end
-            })
-            require('kommentary.config').configure_language("vim", {
+            require('kommentary.config').configure_language({"default", "html", "vim"}, {
                 ignore_whitespace = false,
                 use_consistent_indentation = true,
                 single_line_comment_string = 'auto',
